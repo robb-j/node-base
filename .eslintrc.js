77e90451a59be1efead229909f8e625de7722bdd
Rob Anderson <rob@andrsn.uk>
2018-09-30 12:29:46 +0100

    :star: Finish move to jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,26 +7,17 @@ module.exports = {
     ecmaVersion: 2017
   },
   env: {
-    node: true
+    node: true,
+    jest: true
   },
   extends: 'standard',
   
   // Custom Rules
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    
-    
-    /* Rob's tweaks */
-    'no-unused-vars': 'warn',
+  rules: {
     'no-trailing-spaces': [ 'error', { 'skipBlankLines': true } ]
   },
   
   // Custom globals ... don't use globals
-  "globals": {
+  globals: {
   }
 }
